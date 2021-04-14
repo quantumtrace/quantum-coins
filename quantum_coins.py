187133ff115d44be2bf1816c5abb5eeebc99fe70
Federico <81957489+quantumtrace@users.noreply.github.com>
2021-04-15 00:02:30 +0200

    Update quantum_coins.py

--- quantum_coins.py
+++ quantum_coins.py
@@ -2,7 +2,7 @@
 from qiskit import QuantumCircuit, IBMQ, execute
 
 #Authenticate an account and add for use during this session.
-IBMQ.enable_account("415d26710fbef43d0326deed78f2694f29c849e3336995815fc48125cced1804dfd9c6a9aa9733d7973bb458f40d9e776c168b23e92fbf9710f9178a659453aa")
+IBMQ.enable_account("YOUR_API_TOKEN")
 provider = IBMQ.get_provider(hub='ibm-q')
 
 #Initialize the number of qubits and classical registers
